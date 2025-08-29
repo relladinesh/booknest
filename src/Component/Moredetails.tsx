0fc29368d7f43e07064f9da9efec32ceb5e2f103
relladinesh <150235051+relladinesh@users.noreply.github.com>
2025-08-29 22:04:31 +0530

    Update Moredetails.tsx

--- src/Component/Moredetails.tsx
+++ src/Component/Moredetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { supabase } from "../../supa/Supabaseclient";
+import { supabase } from "../../supa/supabaseclient";
 import { Loader2, ArrowLeft, Mail, Calendar, User } from "lucide-react";
 import { toast } from "react-hot-toast";
 
